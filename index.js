2aaf76554b4e8449a7bd451af9552e145aa2acaa
Andrea <andrea.berrocal95@gmail.com>
2022-05-16 21:29:41 -0700

    ⚡️ Using promises to claw myself out of callback hell

--- index.js
+++ index.js
@@ -1,62 +1,52 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert').strict;
 const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-
-    /*  we are checking if he first parameter (err) strictly equals the second parameter (null)
-        if they do match we will continue
-        if they do not match err != null then the assert will fail
-        when an assert fails it will throw an error and terminate the entire application
-        and will log the error that occurred 
-    */
-
-    assert.strictEqual(err, null);
+MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
 
     console.log('Connected correctly to server');
 
-     //this method will connect us to the database on mongodb server
     const db = client.db(dbname);
 
-    //delete (drop) everything that is in the database
-    db.dropCollection('campsites', (err, result) => {
-        assert.strictEqual(err, null);
+    db.dropCollection('campsites')
+    .then(result => {
         console.log('Dropped Collection:', result);
+    })
+    .catch(err => console.log('No collection to drop.'));
 
-        //insert document into database
-        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test"},
-            'campsites', result => {
-            console.log('Insert Document:', result.ops);
-            
-            //find all documents
-            dboper.findDocuments(db, 'campsites', docs => {
-                console.log('Found Documents:', docs);
+    dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"}, 'campsites')
+    .then(result => {
+        console.log('Insert Document:', result.ops);
 
-                //update document in database
-                dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
-                    { description: "Updated Test Description" }, 'campsites',
-                    result => {
-                        console.log('Updated Document Count:', result.result.nModified);
+        return dboper.findDocuments(db, 'campsites');
+    })
+    .then(docs => {
+        console.log('Found Documents:', docs);
 
-                        //find all documents
-                        dboper.findDocuments(db, 'campsites', docs => {
-                            console.log('Found Documents:', docs);
-                            
-                            //remove document in database
-                            dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
-                                'campsites', result => {
-                                    console.log('Deleted Document Count:', result.deletedCount);
+        return dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+            { description: "Updated Test Description" }, 'campsites');
+    })
+    .then(result => {
+        console.log('Updated Document Count:', result.result.nModified);
 
-                                    client.close();
-                                }
-                            );
-                        });
-                    }
-                );
-            });
-        });
+        return dboper.findDocuments(db, 'campsites');
+    })
+    .then(docs => {
+        console.log('Found Documents:', docs);
+
+        return dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+            'campsites');
+    })
+    .then(result => {
+        console.log('Deleted Document Count:', result.deletedCount);
+
+        return client.close();
+    })
+    .catch(err => {
+        console.log(err);
+        client.close();
     });
-});
+})
+.catch(err => console.log(err));
